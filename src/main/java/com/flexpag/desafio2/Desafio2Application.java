2c948239fe8a139dc1a4176b6831bcbc2f89ebbd
filiperibeiro93 <facribeiro.93@gmail.com>
2022-09-28 16:07:54 -0300

    starting security

--- src/main/java/com/flexpag/desafio2/Desafio2Application.java
+++ src/main/java/com/flexpag/desafio2/Desafio2Application.java
@@ -2,8 +2,12 @@ package com.flexpag.desafio2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableCaching
+@EnableWebSecurity
 public class Desafio2Application {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/flexpag/desafio2/config/security/SecurityConfigurations.java
@@ -0,0 +1,41 @@
+package com.flexpag.desafio2.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfigurations {
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/payments").permitAll()
+                .antMatchers(HttpMethod.GET, "/payments/*").permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin();
+                /*.antMatchers(HttpMethod.POST, "/auth").permitAll()
+                .antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+                .anyRequest().authenticated()
+                .and().csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);*/
+
+        return http.build();
+    }
+}
